f1d3205603c2fbbaabdaeca9ff0c0ccaeca04aab
Rodrigo Machado <rodrigo.krv@gmail.com>
2018-10-24 14:50:11 -0300

    add ios splash screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 import Navigator from './Navigator';
+import SplashScreen from 'react-native-splash-screen';
 
 /*you must change the URI to your local graphql server*/
 const client = new ApolloClient({
@@ -9,6 +10,9 @@ const client = new ApolloClient({
 })
 
 class App extends Component {
+  componentDidMount() {
+    SplashScreen.hide();
+  }
   render () {
     return (
       <ApolloProvider client={ client }>
